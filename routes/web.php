51044b50ad6568c5b16840b8dc40f26d4ae5f1a5
Jollish <106536867+Jollish@users.noreply.github.com>
2023-02-15 17:47:30 +0800

    Completed

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users',[UserController::class, 'index']);
 Route::get( '/user/{id}',[UserController::class, 'show']);
+Route::get('/customers', [CustomerController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
