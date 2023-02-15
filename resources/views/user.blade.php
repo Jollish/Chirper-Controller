83928f1109abfc50b220e597952fda69dab28bad
Jollish <106536867+Jollish@users.noreply.github.com>
2023-02-15 17:26:01 +0800

    first commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chirper_Controller</title>
+</head>
+<body style='background: #013200; text-align: center; color: aliceblue; font-size: 10vh'>
+    Name: {{$name}} 
+    <br>
+    Age: {{$age}}
+    <br>
+    Email: {{$email}}
+    
+</body>
+</html>
